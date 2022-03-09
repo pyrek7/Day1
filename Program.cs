8f00ad6713a5f8df34a4cabb8875bf91e82cabb2
pyrek7 <38038642+pyrek7@users.noreply.github.com>
2022-03-09 11:42:18 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Day1
             for(int i =0;i<25;i++)
             {
                 System.Threading.Thread.Sleep(1000);
-                Console.WriteLine(i)
+                Console.WriteLine(i);
             }
         }
     }
